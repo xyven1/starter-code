f96e4adddb13794b773b110b28f92387106421dc
Ruba Khan <ruba.khan2014@gmail.com>
2023-03-17 15:46:54 -0400

    added a print statement

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,7 +3,9 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
+
     App.launch(App.class, args);
+    System.out.println("Ruba");
   }
 
   // shortcut: psvm
